f15742a45f3a4b1748a0ea169b661b5f04d5dd63
JefferssonSemin <jeffesemin@hotmail.com>
2021-08-02 15:12:02 -0300

    refactor: create specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,26 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  try {
-    createSpecificationService.execute({ name, description });
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-
-  return res.status(201).send();
+  return createCategoryController.handle(req, res);
 });
 
 // specificationsRouter.get("/", (req, res)=>{
