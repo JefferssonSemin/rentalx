ceef899e75983d78e8e1790220b0a336fe6dbb30
JefferssonSemin <jeffesemin@hotmail.com>
2021-08-02 09:47:40 -0300

    chore: add eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) =>{
-  return res.json({success:"asf"})
+app.get("/", (req, res) => res.json({ success: "asf" }));
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
-app.listen(3333, () => console.log('Server is running'));
+app.listen(3333, () => console.log("Server is running"));
