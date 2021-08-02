0e6142fa4ff3366b44fafaaa62217c83f5b43ac6
JefferssonSemin <jeffesemin@hotmail.com>
2021-08-02 11:10:27 -0300

    chore: add SRP and DIP

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,21 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
-
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadyExists) {
-    return res.status(400).json({ error: "Category already exists." });
+  try {
+    createCategoryService.execute({ name, description });
+  } catch (error) {
+    res.status(400).json({ error });
   }
-
-  categoriesRepository.create({ name, description });
 
   return res.status(201).send();
 });
